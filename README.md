a9dbe005ba0b555e2ad62d964a028143e959b8cb
Bryan Jennings <bryanjenningz@gmail.com>
2017-10-07 13:05:12 -0700

    add instructions and example

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-# record-audio
+# Record Audio
+
+A simple audio recording function.
+
+### How to use
+
+The following code will record audio for 3 seconds, then play back the audio that it recorded.
+
+```javascript
+(async () => {
+  const recorder = await recordAudio();
+  recorder.start();
+  await sleep(3000);
+  const audio = await recorder.stop();
+  audio.play();
+})();
+```
+
+### Instructions for running example
+
+Make sure your browser is up to date.
+
+Clone the repo, then open index.html and start talking. You will be recorded for 3 seconds, then your recording will be played back.
---
+++ index.js
@@ -0,0 +1,37 @@
+const recordAudio = () =>
+  new Promise(async resolve => {
+    const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
+    const mediaRecorder = new MediaRecorder(stream);
+    const audioChunks = [];
+
+    mediaRecorder.addEventListener("dataavailable", event => {
+      audioChunks.push(event.data);
+    });
+
+    const start = () => mediaRecorder.start();
+
+    const stop = () =>
+      new Promise(resolve => {
+        mediaRecorder.addEventListener("stop", () => {
+          const audioBlob = new Blob(audioChunks);
+          const audioUrl = URL.createObjectURL(audioBlob);
+          const audio = new Audio(audioUrl);
+          const play = () => audio.play();
+          resolve({ audioBlob, audioUrl, play });
+        });
+
+        mediaRecorder.stop();
+      });
+
+    resolve({ start, stop });
+  });
+
+const sleep = time => new Promise(resolve => setTimeout(resolve, time));
+
+(async () => {
+  const recorder = await recordAudio();
+  recorder.start();
+  await sleep(3000);
+  const audio = await recorder.stop();
+  audio.play();
+})();
